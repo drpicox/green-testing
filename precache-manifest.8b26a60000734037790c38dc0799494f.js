98996daad606f029d00312e8b0f0d1a099312353
David Rodenas Pico <davidrodenas+gh@gamil.com>
2021-11-11 15:19:21 +0100

    Updates

---
+++ precache-manifest.8b26a60000734037790c38dc0799494f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa76b28195df98c868a8a99c3414898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3a83cb7d49c9385d609",
+    "url": "/static/css/main.c6c97f54.chunk.css"
+  },
+  {
+    "revision": "3e98b46cf5407306a318",
+    "url": "/static/js/2.e23956a2.chunk.js"
+  },
+  {
+    "revision": "efc83a6c2a6b67a6cbdc5586749d90b1",
+    "url": "/static/js/2.e23956a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a83cb7d49c9385d609",
+    "url": "/static/js/main.2dd36e46.chunk.js"
+  },
+  {
+    "revision": "21499a309e3e4531265f",
+    "url": "/static/js/runtime-main.c0f0c66a.js"
+  },
+  {
+    "revision": "6a5a97b5379ae8d47a908686e226cc5a",
+    "url": "/static/media/grass.6a5a97b5.jpeg"
+  }
+]);
